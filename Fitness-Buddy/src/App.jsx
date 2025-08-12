bcae15b0819d386b54baa0b62616d233b06650b8
Fiona Wambua <fionawambua20@gmail.com>
2025-08-12 15:41:30 +0300

    initilization of the login page and its components

--- Fitness-Buddy/src/App.jsx
+++ Fitness-Buddy/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<WelcomePage />} />
+      <Route path="/LoginPage" element={<LoginPage/>}></Route>
     </Routes>
   );
 }
---
+++ Fitness-Buddy/src/pages/LoginPage.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "../styles/LoginPage.css";
+
+const LoginPage = () => {
+    return (
+        <div className="loginpage-container">
+            <form action="">
+                <h1> Fitness Buddy </h1>
+                 <h2>Create your Account</h2>
+                <div class="input-box">
+                    <input type="text" placeholder="Username" ></input>
+                    <i class="bx bxs-user"></i>
+                </div>
+                <div class="input-box">
+                    <input type="password"
+                        placeholder="Password"></input>
+                    <i class="bx bxs=lock-alt"></i>
+                </div>
+                <div class="rem-forgot">
+                    <label>
+                        <input type="checkbox" />Remember Me </label>
+                    <a href="#">Forgot Password</a>
+                    </div>
+                <button type="submit" class="btn">Login</button>
+                <h3>or sign up with</h3>
+                 <div className="social-login">
+          <button className="google-btn">
+          <img src="/google-icon.png" alt="Google" /> Continue with Google
+        </button>
+        <button className="facebook-btn">
+          <img src="/facebook-icon.png" alt="Facebook" /> Continue with Facebook
+        </button>
+        <button className="twitter-btn">
+          <img src="/twitter-icon.png" alt="Twitter" /> Continue with Twitter
+                    </button>
+                    </div>
+                </form>
+            </div>
+    );
+};
+
+export default LoginPage;

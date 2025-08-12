8201cd7d37289320991ed70a257faaf278877e56
Fiona Wambua <fionawambua20@gmail.com>
2025-08-12 14:13:02 +0300

    creation of welcome page

--- Fitness-Buddy/src/App.jsx
+++ Fitness-Buddy/src/App.jsx
@@ -1,3 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import WelcomePage
+import WelcomePage from "./pages/WelcomePage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+    </Routes>
+  );
+}
+
+export default App;

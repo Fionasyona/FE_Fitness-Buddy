86f8016be34826bd02a942e36af4a207157e8843
Fiona Wambua <fionawambua20@gmail.com>
2025-08-12 18:46:54 +0300

    new additions in loginpage for the social icons

--- Fitness-Buddy/src/pages/LoginPage.jsx
+++ Fitness-Buddy/src/pages/LoginPage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "../styles/LoginPage.css";
+import googleIcon from '../assets/images/iconsgoogle.png';
+import facebookIcon from "../assets/images/iconsfacebook.png";
+import twitterIcon from "../assets/images/iconstwitter.png";
 
 const LoginPage = () => {
     return (
@@ -9,13 +12,11 @@ const LoginPage = () => {
                 <div class="input-box">
                     <input type="text" placeholder="Username" required
                     />
-                    <i class="bx bxs-user"></i>
                 </div>
                 <div class="input-box">
                     <input type="password"
                         placeholder="Password" required
                     />
-                    <i class="bx bxs-lock-alt"></i>
                 </div>
                 <div class="rem-forgot">
                     <label>
@@ -26,13 +27,13 @@ const LoginPage = () => {
                 <h3>or sign up with</h3>
                  <div className="social-login">
           <button className="google-btn">
-          <img src="/google-icon.png" alt="Google" /> Continue with Google
+          <img src={googleIcon} alt="Google" /> Continue with Google
         </button>
         <button className="facebook-btn">
-          <img src="/facebook-icon.png" alt="Facebook" /> Continue with Facebook
+          <img src={facebookIcon} alt="Facebook" /> Continue with Facebook
         </button>
         <button className="twitter-btn">
-          <img src="/twitter-icon.png" alt="Twitter" /> Continue with Twitter
+          <img src={twitterIcon} alt="Twitter" /> Continue with Twitter
                     </button>
                     </div>
                 </form>
